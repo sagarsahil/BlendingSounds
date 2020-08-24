a5195ecd3aa6d68b729fe01974a20ee8eb447978
Sahil <sahil.sagar@gmail.com>
2020-08-23 21:02:29 -0400

    Intial Checkin

---
+++ script.js
@@ -0,0 +1,63 @@
+function update(){
+    $('#homepageItems').val("");
+    $('#indicators').val("");
+    var allwords = $('#inputTextArea').val();
+    if (allwords=="") return;
+    
+    allwordssplit=allwords.split("\n")
+   
+    var response='';
+    var indicator='';
+    for (var i = 0; i < allwordssplit.length; i++) {
+        var str = allwordssplit[i];
+        var arr;
+        if(str.indexOf("-")==-1)
+        {
+            arr = str.split("");
+        }
+        else
+        {
+            arr = str.split("-");
+        }
+        
+        var chars;
+        response += '<div class="item btn-toolbar"> ';
+        for (chars = 0; chars < arr.length; chars++) {
+            if(chars==1)
+            {
+                response += '<button type="button" class="btn-warning btn-lg">'+arr[chars] + "</button>";
+            }
+            else
+            {
+                response += '<button type="button" class="btn-primary btn-lg">'+arr[chars] + "</button>";    
+            }
+            
+            }
+        response +=' </center></div>';
+        indicator += '<li data-target="#myCarousel" data-slide-to="'+i+'"></li>';
+    }
+    $('#homepageItems').html(response);
+    $('#indicators').html(indicator);
+    $('.item').first().addClass('active');
+    $('.carousel-indicators > li').first().addClass('active');
+    $("#myCarousel").carousel();
+    hide();
+ };
+
+ function hide(){
+    if($('#btnhide').text()=="Hide")
+    {
+        $('#inputTextArea').hide();
+        $('#btngenerate').hide()
+        $('#instructions').hide()
+        
+        $('#btnhide').text("Show")
+    }
+    else
+    {
+        $('#inputTextArea').show();
+        $('#btngenerate').show();
+        $('#instructions').show();
+        $('#btnhide').text("Hide");
+    }
+ }
